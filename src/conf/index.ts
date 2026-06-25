54a52dbea74073c232f90aeefef794a59d079e66
builtbysuraj <surajgupta3553@gmail.com>
2026-06-25 12:49:38 +0530

    chore: Add new user and mapping

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -69,9 +69,10 @@ export const USERS_MAP: Record<string, string> = {
   '3899927000000615348': 'Ashok M',
   '3899927000000964875': 'Suraj Gupta',
   '3899927000000882594': 'Myisa Beiucy',
-  '3899927000000723465': 'Kaveri Metri',
   '3899927000000201013': 'Anslem Prathap',
   '3899927000005965002': 'Subhasini TS',
+  '3899927000000503894': 'Saran D',
+  '3899927000000135140': 'Prathamesh Prakash',
 }
 
 // HR Team User ID Registry constants
@@ -89,4 +90,5 @@ export const MANAGER_USER_IDS = [
   '3899927000005114004', // Manjunath
   '3899927000000851906', // Prathap
   '3899927000000488938', // Nagaraj
+  '3899927000000979220', // Nandini
 ]
